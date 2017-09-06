e38e518005fd29c30ef57f6125249f48b0dd0cfe
amit kasturi <amit.kasturi@gmail.com>
2017-09-06 17:48:32 +0530

    executePhp helper function added.

--- app/src/main/java/com/example/amkastur/myphpexample/BackgroundWorker.java
+++ app/src/main/java/com/example/amkastur/myphpexample/BackgroundWorker.java
@@ -12,6 +12,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
@@ -24,112 +25,96 @@ import java.net.URLEncoder;
 
 public class BackgroundWorker extends AsyncTask<String, Object, String> {
 
-    Context mContext ;
-    AlertDialog mAlerDialog;
+    private Context mContext ;
+    private AlertDialog mAlerDialog;
 
     BackgroundWorker(Context context){
         mContext = context;
     }
 
-    //String executePhp(String url, String postData)
+    private String executePhp(String urlStr, String postData) {
+
+        try {
+            URL url = new URL(urlStr);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+            httpURLConnection.setRequestMethod("POST");
+            httpURLConnection.setDoInput(true);
+            httpURLConnection.setDoOutput(true);
+
+            OutputStream outputStream = httpURLConnection.getOutputStream();
+            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+
+            bufferedWriter.write(postData);
+            bufferedWriter.flush();
+            bufferedWriter.close();
+            outputStream.close();
+
+            InputStream inputStream = httpURLConnection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
+            String result=null;
+            String line;
+
+            while ((line = bufferedReader.readLine()) != null) {
+                result += line;
+            }
+            bufferedReader.close();
+            inputStream.close();
+            httpURLConnection.disconnect();
+
+            return result;
+        }  catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
     @Override
     protected String doInBackground(String... voids) {
-        String type = (String)voids[0];
-        String loingUrl = "http://helpingapps.in/php/login.php";
+        String type = voids[0];
+        String loginUrl = "http://helpingapps.in/php/login.php";
         String registerUrl = "http://helpingapps.in/php/register.php";
         String fetchPetUrl = "http://helpingapps.in/php/fetchPet.php";
+        String result = null;
 
-        if(type.equals("login")){
+        if (type.equals("login")) {
+            String username =  voids[1];
+            String password =  voids[2];
+            String postData = null;
+
             try {
-                URL url =  new URL(loingUrl);
-                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
-
-                httpURLConnection.setRequestMethod("POST");
-                httpURLConnection.setDoInput(true);
-                httpURLConnection.setDoOutput(true);
-
-                OutputStream outputStream = httpURLConnection.getOutputStream();
-                BufferedWriter bufferedWriter =  new BufferedWriter(new OutputStreamWriter(outputStream,"UTF-8"));
-
-                String username = (String) voids[1];
-                String password = (String) voids[2];
-                String post_data = URLEncoder.encode("user_name","UTF-8") + "=" + URLEncoder.encode(username,"UTF-8") + "&"
-                        + URLEncoder.encode("password","UTF-8") + "=" + URLEncoder.encode(password,"UTF-8");
-
-                bufferedWriter.write(post_data);
-                bufferedWriter.flush();
-                bufferedWriter.close();
-                outputStream.close();
-
-                InputStream inputStream = httpURLConnection.getInputStream();
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
-                String result="";
-                String line="";
-
-                while((line = bufferedReader.readLine()) != null){
-                    result += line;
-                }
-                bufferedReader.close();
-                inputStream.close();
-                httpURLConnection.disconnect();
-
-                return result;
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (ProtocolException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-        } else if(type.equals("register")) {
-            try {
-                //URL url = new URL(registerUrl);
-                URL url = new URL(fetchPetUrl);
-                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-
-                httpURLConnection.setRequestMethod("POST");
-                httpURLConnection.setDoInput(true);
-                httpURLConnection.setDoOutput(true);
-
-                OutputStream outputStream = httpURLConnection.getOutputStream();
-                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
-
-                String username = (String) voids[1];
-                String owner = (String) voids[2];
-                String password = (String) voids[3];
-
-
-                String post_data = URLEncoder.encode("user_name", "UTF-8") + "=" + URLEncoder.encode(username, "UTF-8")+ "&"
-                        + URLEncoder.encode("owner", "UTF-8") + "=" + URLEncoder.encode(owner, "UTF-8")  + "&"
+                postData = URLEncoder.encode("user_name", "UTF-8") + "=" + URLEncoder.encode(username, "UTF-8") + "&"
                         + URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(password, "UTF-8");
-
-                bufferedWriter.write(post_data);
-                bufferedWriter.flush();
-                bufferedWriter.close();
-                outputStream.close();
-
-                InputStream inputStream = httpURLConnection.getInputStream();
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
-                String result = "";
-                String line = "";
-
-                while ((line = bufferedReader.readLine()) != null) {
-                    result += line;
-                }
-                bufferedReader.close();
-                inputStream.close();
-                httpURLConnection.disconnect();
-
-                return result;
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (ProtocolException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
+            } catch (UnsupportedEncodingException e) {
                 e.printStackTrace();
             }
+
+            result = executePhp(loginUrl, postData);
+
+            return result;
+
+        } else if (type.equals("register")) {
+
+            String username =  voids[1];
+            String owner =   voids[2];
+            String password =  voids[3];
+            String postData = null;
+
+            try {
+                postData = URLEncoder.encode("user_name", "UTF-8") + "=" + URLEncoder.encode(username, "UTF-8") + "&"
+                        + URLEncoder.encode("owner", "UTF-8") + "=" + URLEncoder.encode(owner, "UTF-8") + "&"
+                        + URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(password, "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+
+            result = executePhp(fetchPetUrl, postData);
+
+            return result;
         }
 
         return null;
