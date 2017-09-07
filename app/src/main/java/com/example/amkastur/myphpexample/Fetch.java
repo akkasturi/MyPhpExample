9f3c37cec4ceb4d6d43b9a4c5ce4b211d89ff107
amit kasturi <amit.kasturi@gmail.com>
2017-09-07 23:33:34 +0530

    Jason Parsing. List view :)

--- app/src/main/java/com/example/amkastur/myphpexample/Fetch.java
+++ app/src/main/java/com/example/amkastur/myphpexample/Fetch.java
@@ -1,10 +1,12 @@
 package com.example.amkastur.myphpexample;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -14,6 +16,8 @@ import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Fetch extends AppCompatActivity {
 
@@ -30,7 +34,7 @@ public class Fetch extends AppCompatActivity {
 
         String petRows;
 
-        FetchBgWorker backgroundWorker = new FetchBgWorker();
+        FetchBgWorker backgroundWorker = new FetchBgWorker(this);
         backgroundWorker.execute("fetch");
 
     }
@@ -41,6 +45,9 @@ public class Fetch extends AppCompatActivity {
     }
 
     private class FetchBgWorker extends AsyncTask<String, Object, String> {
+        Context mContext;
+
+        FetchBgWorker(Context context) {mContext = context;}
 
         @Override
         protected String doInBackground(String... voids) {
@@ -65,21 +72,31 @@ public class Fetch extends AppCompatActivity {
         protected void onPostExecute(String aVoid) {
             //mFetchText.setText(aVoid);
             String rows="";
-            
+            ArrayList<String> items = new ArrayList<String>();
+
             try {
                 JSONArray jArray = new JSONArray(aVoid);
 
+
                 for(int i=0;i<jArray.length();i++){
                     JSONObject json_data = jArray.getJSONObject(i);
-                    rows = "name : " + json_data.getString("name") +
-                            ", owner : " + json_data.getString("owner") +
-                            ", password: " + json_data.getString("password");
 
+                    /*rows = "name : " + json_data.getString("name") +
+                            ", owner : " + json_data.getString("owner") +
+                            ", password: " + json_data.getString("password");*/
+                    rows =  json_data.getString("name") +
+                            ", " + json_data.getString("owner") +
+                            ", " + json_data.getString("password");
+                    items.add(rows);
                 }
             } catch (JSONException e) {
                 e.printStackTrace();
             }
-            mFetchText.setText(rows);
+            ArrayAdapter<String> mArrayAdapter = new ArrayAdapter<String>(mContext,
+                    android.R.layout.simple_list_item_1, items);
+
+            mPetsList.setAdapter(mArrayAdapter);
+            //mFetchText.setText(items.toString());
         }
 
         @Override
